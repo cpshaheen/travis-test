8e377a84de3e0e3dbe94a4f74aa9dcf7cfb156f0
cpshaheen <46461452+cpshaheen@users.noreply.github.com>
2019-01-25 15:56:41 -0800

    adding the actual bug

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,4 +1,4 @@
-ublic class helloworld {
+public class helloworld {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World!!! GOAT!");
